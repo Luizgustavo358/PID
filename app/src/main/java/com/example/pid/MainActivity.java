6872988d95e15e0ce1656a208f1677e59b4c7f1c
Álefe Lucas <alefelucas.contato@gmail.com>
2019-10-01 19:10:14 -0300

    included grayscale and threshold

--- app/src/main/java/com/example/pid/MainActivity.java
+++ app/src/main/java/com/example/pid/MainActivity.java
@@ -18,6 +18,7 @@ import android.view.SurfaceView;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
@@ -34,10 +35,13 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
 import org.opencv.core.Mat;
 import org.opencv.imgproc.Imgproc;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -51,6 +55,7 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
 
     Animation hideLayout;
     Animation hideShadow;
+    Button button;
 
     Animation showLayout;
     Animation showShadow;
@@ -88,9 +93,20 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
      */
     private void setListeners() {
         option1Layout.setOnClickListener(this::dispatchTakePictureIntent);
-
         option2Layout.setOnClickListener(this::choosePictureFromSomewhere);
+        button.setOnClickListener(v -> {
+            Mat mat = new Mat();
+            Bitmap bmp32 = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+            Utils.bitmapToMat(bmp32, mat);
+            Mat destination = new Mat();
+            Imgproc.cvtColor(mat, destination, Imgproc.COLOR_RGB2GRAY);
+            Mat destination2 = new Mat();
+            Imgproc.adaptiveThreshold(destination, destination2, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 11, 7);
 
+            Utils.matToBitmap(destination2, bitmap);
+            storeImage(bitmap);
+            imageView.setImageBitmap(bitmap);
+        });
     }
 
     private void choosePictureFromSomewhere(View view) {
@@ -234,6 +250,59 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         this.sendBroadcast(mediaScanIntent);
     }
 
+    /** Create a File for saving an image or video */
+    private  File getOutputMediaFile() throws IOException {
+        // To be safe, you should check that the SDCard is mounted
+        // using Environment.getExternalStorageState() before doing this.
+        File mediaStorageDir = new File(
+                Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_PICTURES
+                ),
+                "PID"
+        );
+        mediaStorageDir.mkdirs();
+
+
+
+
+        // This location works best if you want the created images to be shared
+        // between applications and persist after your app has been uninstalled.
+
+        // Create the storage directory if it does not exist
+
+        // Create a media file name
+        File mediaFile;
+
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(new Date());
+        String imageFileName = "JPEG" + timeStamp + "_";
+        mediaFile =File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                mediaStorageDir      /* directory */
+        );
+        return mediaFile;
+    }
+
+    private void storeImage(Bitmap image) {
+
+        try {
+            File pictureFile = getOutputMediaFile();
+            if (pictureFile == null) {
+                Log.d(TAG,
+                        "******************* Error creating media file, check storage permissions: ");// e.getMessage());
+                return;
+            }
+            FileOutputStream fos = new FileOutputStream(pictureFile);
+            image.compress(Bitmap.CompressFormat.JPEG, 90, fos);
+            fos.close();
+            System.out.println("4$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
+        } catch (FileNotFoundException e) {
+            Log.d(TAG, "**************** File not found: " + e.getMessage());
+        } catch (IOException e) {
+            Log.d(TAG, "**************** Error accessing file: " + e.getMessage());
+        }
+    }
+
     private void setPic() {
         // Get the dimensions of the View
         int targetW = imageView.getWidth();
@@ -268,6 +337,7 @@ public class MainActivity extends AppCompatActivity implements CvCameraViewListe
         option2Layout = findViewById(R.id.option2);
         fabPhoto = findViewById(R.id.fab_photo);
         imageView = findViewById(R.id.imageView);
+        button = findViewById(R.id.button);
     }
 
 
