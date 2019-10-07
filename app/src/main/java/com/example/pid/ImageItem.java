1ed27866dc9340f974b7136e1915fbce921f9503
Álefe Lucas <alefelucas.contato@gmail.com>
2019-10-06 22:47:04 -0300

    changing adapter

--- app/src/main/java/com/example/pid/ImageItem.java
+++ app/src/main/java/com/example/pid/ImageItem.java
@@ -7,8 +7,31 @@ import java.io.File;
 
 public class ImageItem {
 
-    File file;
-    ImageView imageView;
-    Bitmap bitmap;
+    private File file;
+    private ImageView imageView;
+    private Bitmap bitmap;
 
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
+    }
+
+    public ImageView getImageView() {
+        return imageView;
+    }
+
+    public void setImageView(ImageView imageView) {
+        this.imageView = imageView;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
 }
--- app/src/main/java/com/example/pid/ImagensAdapter.java
+++ app/src/main/java/com/example/pid/ImagensAdapter.java
@@ -1,7 +1,9 @@
 package com.example.pid;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,28 +12,45 @@ import java.util.ArrayList;
 
 public class ImagensAdapter extends RecyclerView.Adapter<ImagensAdapter.ImagensViewHolder> {
 
+    private static final int IMAGE = 0;
     private ArrayList<ImageItem> imageItems;
+
+    public ImagensAdapter() {
+        this.imageItems = new ArrayList<>();
+    }
 
     @NonNull
     @Override
     public ImagensViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        if (viewType == IMAGE) {
+            ImagensViewHolder holder = new ImagensViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.recycler_item_1, parent, false));
+            return holder;
+        }
+        throw new IllegalStateException();
     }
 
     @Override
     public void onBindViewHolder(@NonNull ImagensViewHolder holder, int position) {
+        holder.imageView.setImageBitmap(imageItems.get(position).getBitmap());
+    }
 
+    @Override
+    public int getItemViewType(int position) {
+        return IMAGE;
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return imageItems.size();
     }
 
     class ImagensViewHolder extends RecyclerView.ViewHolder{
 
+        public ImageView imageView;
+
         public ImagensViewHolder(@NonNull View itemView) {
             super(itemView);
+            imageView = itemView.findViewById(R.id.imageView);
         }
     }
 }
