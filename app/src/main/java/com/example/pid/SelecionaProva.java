0b42f6578bbea151f1b99278ee84dbde09b9ba63
Luiz Gustavo <luiz.braganca@sga.pucminas.br>
2019-11-24 16:37:27 -0300

    Segunda Tela - Prova em Branco

---
+++ app/src/main/java/com/example/pid/SelecionaProva.java
@@ -0,0 +1,14 @@
+package com.example.pid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelecionaProva extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleciona_prova);
+    }
+}
