9de3acd545e14394e81f38059ad50edd2c43fdb8
Luiz Gustavo <luiz.braganca@sga.pucminas.br>
2019-11-24 17:37:37 -0300

    Atualização

--- app/src/main/java/com/example/pid/TelaInicial.java
+++ app/src/main/java/com/example/pid/TelaInicial.java
@@ -2,7 +2,9 @@ package com.example.pid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TelaInicial extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class TelaInicial extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+    }
+
+    public void provaEmBranco(View view) {
+        Intent intent = new Intent(this, SelecionaProva.class);
+        startActivity(intent);
     }
 }
