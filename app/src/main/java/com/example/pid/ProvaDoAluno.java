b18a4b7d5e04517fd8b61c38cc65564ce3d8d9d1
Luiz Gustavo <luiz.braganca@sga.pucminas.br>
2019-11-27 23:29:41 -0300

    Tela prova aluno

---
+++ app/src/main/java/com/example/pid/ProvaDoAluno.java
@@ -0,0 +1,25 @@
+package com.example.pid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class ProvaDoAluno extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prova_do_aluno);
+    }
+
+    /**
+     * Volta para tela inicial.
+     * @param view
+     */
+    public void telaInicial(View view) {
+        Intent intent = new Intent(this, TelaInicial.class);
+        startActivity(intent);
+    }
+}
